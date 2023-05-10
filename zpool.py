bbf63306bce3740755aab4a13edd6f9eec952905
LukeBeirne <120753966+LukeBeirne@users.noreply.github.com>
2023-05-09 23:40:58 -0400

    Add files via upload

---
+++ zpool.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+import subprocess
+import libzfs_core
+
+#
+# Setting up libzfs_core import
+#
+# PYTHONPATH environment variable:
+# export PYTHONPATH=zfs/contrib/pyzfs
+#
+# Python3 should recognize libzfs_core
+# as a package to import
+#
+
+print('hello world!')
